a14ec2cc81f6ea7a732c9a81c15c51885e4df68c
Maksudul Haque <saad.mk112@gmail.com>
2021-11-16 14:39:32 +0000

    Update Actions

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-//Load express module with `require` directive
-var express = require('express')
-var app = express()
-//Define port
-var port = 3000
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-//Launch listening server on port 3000
-app.listen(port, function () {
-  console.log(`app listening on port ${port}!`)
-})
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
